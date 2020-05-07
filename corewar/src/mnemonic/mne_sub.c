40cc12b6e519966b1d6cd9aed3fc03d563719072
planchot killian <pkillian@localhost.localdomain>
2020-05-07 15:49:58 +0200

    [ADD] mnemonic add and sub

---
+++ corewar/src/mnemonic/mne_sub.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** mnemonic sub
+** File description:
+** corewar
+*/
+
+#include "corewar.h"
+
+int mne_sub(champ_t *champ, battle_t *battle)
+{
+    int fst_param = get_value(champ, battle);
+    int scd_param = get_value(champ, battle);
+    int thd_param = get_value(champ, battle);
+
+    if (is_register(fst_param) == 84 || is_register(fst_param) == 84
+    || is_register(thd_param) == 84)
+        return 84;
+    champ->reg[thd_param - 1] = champ->reg[fst_param - 1]
+    - champ->reg[scd_param - 1];
+    champ->carry = 1;
+    return (0);
+}
