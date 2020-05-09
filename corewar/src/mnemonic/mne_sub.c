ab6144bb405e1eb4f3777ed85e16354f60b2d6a2
planchot killian <pkillian@localhost.localdomain>
2020-05-09 16:00:14 +0200

    [ADD] mnemonic add and sub remake

--- corewar/src/mnemonic/mne_sub.c
+++ corewar/src/mnemonic/mne_sub.c
@@ -9,13 +9,21 @@
 
 int mne_sub(champ_t *champ, battle_t *battle)
 {
-    int fst_param = battle->mem[champ->pc + 2];
-    int scd_param = battle->mem[champ->pc + 3];
-    int thd_param = battle->mem[champ->pc + 4];
+    int param[4] = get_param_type(&param,
+    battle->mem[(champ->pc + 1) % MEM_SIZE]);
+    int fst_param = battle->mem[(champ->pc + 2) % MEM_SIZE];
+    int scd_param = battle->mem[(champ->pc + 3) % MEM_SIZE];
+    int thd_param = battle->mem[(champ->pc + 4) % MEM_SIZE];
 
+    if (param[0] != T_REG || param[1] != T_REG || param[2] != T_REG) {
+        champ->pc += 1;
+        return 0;
+    }
     if (is_register(fst_param) == 84 || is_register(fst_param) == 84
-    || is_register(thd_param) == 84)
-        return 84;
+    || is_register(thd_param) == 84) {
+        champ->pc += 5;
+        return 0;
+    }
     champ->reg[thd_param - 1] = champ->reg[fst_param - 1]
     - champ->reg[scd_param - 1];
     champ->carry = 1;
