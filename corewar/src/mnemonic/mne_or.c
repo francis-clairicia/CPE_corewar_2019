45a0c1223725911e0a17f9268be106c606eb4b70
planchot killian <pkillian@localhost.localdomain>
2020-05-09 17:04:42 +0200

    [ADD] remake of mnemonic and, or, xor

--- corewar/src/mnemonic/mne_or.c
+++ corewar/src/mnemonic/mne_or.c
@@ -7,19 +7,27 @@
 
 #include "corewar.h"
 
-int mne_or(champ_t *champ, battle_t *battle)
+int mne_or(champ_t *chp, battle_t *bat)
 {
-    int fst_param = get_value(champ, battle);
-    int scd_param = get_value(champ, battle);
-    int thd_param = get_value(champ, battle);
+    int param[4] = get_param_type(&param, bat->mem[(chp->pc + 1) % MEM_SIZE]);
+    int idx = chp->pc + 1;
+    int fst_param = 0;
+    int scd_param = 0;
+    int thd_param = 0;
 
-    if (is_register(thd_param) == 84)
-        return 84;
-    if (is_register(fst_param) == 1)
-        fst_param = champ->reg[fst_param - 1];
-    if (is_register(scd_param) == 1)
-        scd_param = champ->reg[scd_param - 1];
-    champ->reg[thd_param - 1] = fst_param | scd_param;
-    champ->carry = 1;
+    if (param[0] == 0 || param[1] == 0 || param[2] != T_REG) {
+        chp->pc += 1;
+        return 0;
+    }
+    fst_param = get_three_value(bat, chp, &idx, param[0]);
+    scd_param = get_three_value(bat, chp, &idx, param[1]);
+    if (idx == -1)
+        chp->pc += 1;
+    if (is_register(bat->mem[(idx + 1) % MEM_SIZE]) == 1 && idx != -1) {
+        thd_param = bat->mem[(idx + 1) % MEM_SIZE];
+        chp->reg[thd_param - 1] = fst_param | scd_param;
+        chp->carry = 1;
+        chp->pc = idx + 1;
+    }
     return 0;
 }
