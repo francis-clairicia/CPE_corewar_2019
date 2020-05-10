b50385606fbdcc623a6d51922eec52b398c22091
alacoste <alexandre.lacoste@epitech.eu>
2020-05-10 18:27:16 +0200

    [MOD] handle error coding style

--- corewar/src/mnemonic/mne_or.c
+++ corewar/src/mnemonic/mne_or.c
@@ -20,8 +20,7 @@ int mne_or(champ_t *chp, battle_t *bat)
     if (param[0] == 0 || param[1] == 0 || param[2] != T_REG) {
         chp->pc += 1;
         return 0;
-    }
-    fst_param = get_three_value(bat, chp, &idx, param[0]);
+    } fst_param = get_three_value(bat, chp, &idx, param[0]);
     scd_param = get_three_value(bat, chp, &idx, param[1]);
     if (is_register(bat->mem[(idx + 1) % MEM_SIZE]) == 1 && idx != -1) {
         thd_param = bat->mem[(idx + 1) % MEM_SIZE];
