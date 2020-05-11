382231c5202fe89e10b233a41d1759dfe907d16a
planchot killian <pkillian@localhost.localdomain>
2020-05-11 17:02:14 +0200

    [ADD] mnemonic sti

--- corewar/src/mnemonic/mne_sti.c
+++ corewar/src/mnemonic/mne_sti.c
@@ -6,21 +6,75 @@
 */
 
 #include "corewar.h"
+#include "mymacros.h"
 
-int mne_sti(champ_t *champ, battle_t *battle)
+int get_thd_value_sti(champ_t *chp, battle_t *bat, int param, int *idx)
 {
-    //int to_adress = 0;
-    //int fst_param = get_value(champ, battle);
-    //int scd_param = get_value(champ, battle);
-    //int thd_param = get_value(champ, battle);
+    int nb = 0;
+    int scd_value = 0;
 
-    //if (is_register(fst_param) == 84)
-    //    return 84;
-    //if (is_register(scd_param) == 1)
-    //    scd_param = champ->reg[scd_param - 1];
-    //if (is_register(thd_param) == 1)
-    //    thd_param = champ->reg[thd_param - 1];
-    //to_adress = champ->pc + (scd_param % IDX_MOD) + (thd_param % IDX_MOD);
-    //write_in_memory(champ->reg[fst_param - 1], to_adress, battle); // write in memory(content to write, to adress, battle with memory);
+    if (*idx == -1)
+        return scd_value;
+    if (param == T_REG) {
+        if (is_register(bat->mem[(*idx + 1) % MEM_SIZE]) == 1) {
+            scd_value = chp->reg[bat->mem[(*idx + 1) % MEM_SIZE] - 1];
+            *idx += 1;
+        } else
+            *idx = -1;
+    }
+    if (param == T_DIR) {
+        scd_value = read_from_mem(bat, *idx + 1, IND_SIZE);
+        *idx += 2;
+    }
+    return scd_value;
+}
+
+int get_scd_value_sti(champ_t *chp, battle_t *bat, int param, int *idx)
+{
+    int nb = 0;
+    int fst_value = 0;
+
+    if (param == T_REG) {
+        if (is_register(bat->mem[(*idx + 1) % MEM_SIZE]) == 1) {
+            fst_value = chp->reg[bat->mem[(*idx + 1) % MEM_SIZE] - 1];
+            *idx += 1;
+        } else
+            *idx = -1;
+    }
+    if (param == T_DIR) {
+        fst_value = read_from_mem(bat, *idx + 1, IND_SIZE);
+        *idx += 2;
+    }
+    if (param == T_IND) {
+        nb = read_from_mem(bat, *idx + 1, IND_SIZE);
+        fst_value = read_from_mem(bat, chp->pc + nb, IND_SIZE);
+        *idx += 2;
+    }
+    return fst_value;
+}
+
+int mne_sti(champ_t *chp, battle_t *bat)
+{
+    int *param = get_param_type(bat->mem[(chp->pc + 1) % MEM_SIZE]);
+    int idx = chp->pc + 1;
+    int fst_param = 0;
+    int scd_param = 0;
+    int thd_param = 0;
+
+    ICHECK(param);
+    if (param[0] != T_REG || param[1] == 0
+    || param[2] == T_IND || param[2] == 0) {
+        chp->pc += 1;
+        return 0;
+    }
+    if (is_register(bat->mem[(idx + 1) % MEM_SIZE])) {
+        fst_param = chp->reg[bat->mem[(idx + 1) % MEM_SIZE] - 1];
+        scd_param = get_scd_value_sti(chp, bat, param[1], idx + 2);
+        thd_param = get_thd_value_sti(chp, bat, param[2], idx + 1);
+        if (idx != -1)
+            add_parameter(bat->mem, fst_param, 4,
+            chp->pc + (scd_param + thd_param) % IDX_MOD);
+    }
+    chp->pc += param[0] + param[1] + param[2] + 2;
     return 0;
 }
