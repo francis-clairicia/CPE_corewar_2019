6e4b9a80ee01f6cdda8de3cad872f8a00801593d
alacoste <alexandre.lacoste@epitech.eu>
2020-05-17 12:00:20 +0200

    [MOD] handle indirect for sti

--- corewar/src/mnemonic/mne_sti.c
+++ corewar/src/mnemonic/mne_sti.c
@@ -8,14 +8,17 @@
 #include "corewar.h"
 #include "mymacros.h"
 
-static int get_index(args_type_t type, int value, int *reg)
+static int get_index(args_type_t type, int value,
+                    champ_t *champ, unsigned char *mem)
 {
     int index = 0;
 
     if (type == T_REG)
-        index = reg[value - 1];
+        index = champ->reg[value - 1];
     if (type == T_DIR)
         index = value;
+    if (type == T_IND)
+        index = read_from_mem(mem, champ->pc + (value % IDX_MOD), REG_SIZE);
     return (index);
 }
 
@@ -30,8 +33,8 @@ int mne_sti(param_t const *params, champ_t *champ, battle_t *battle)
     ICHECK(champ)
     ICHECK(battle)
     register_value = champ->reg[params->value[0] - 1];
-    first = get_index(params->type[1], params->value[1], champ->reg);
-    second = get_index(params->type[2], params->value[2], champ->reg);
+    first = get_index(params->type[1], params->value[1], champ, battle->mem);
+    second = get_index(params->type[2], params->value[2], champ, battle->mem);
     idx = champ->pc + ((first + second) % IDX_MOD);
     add_parameter(battle->mem, register_value, REG_SIZE, idx);
     return 0;
